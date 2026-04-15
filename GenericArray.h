4a2df98f3ed2d1e2e2c2ad324126a76da925b0b8
Yidafiren <alexsong146@gmail.com>
2026-04-15 11:40:04 -0600

    First commit

---
+++ GenericArray.h
@@ -0,0 +1,131 @@
+#ifndef GENERICARRAY_H
+#define GENERICARRAY_H
+
+#include <iostream>
+
+template <typename T>
+class GenericArray
+{
+private:
+    T* data;
+    int currentSize;
+
+public:
+    GenericArray()
+    {
+        data = nullptr;
+        currentSize = 0;
+    }
+
+    ~GenericArray()
+    {
+        delete[] data;
+    }
+
+    void addElement(T value)
+    {
+        T* newData = new T[currentSize + 1];
+
+        for (int i = 0; i < currentSize; i++)
+        {
+            newData[i] = data[i];
+        }
+
+        newData[currentSize] = value;
+
+        delete[] data;
+        data = newData;
+        currentSize++;
+    }
+
+    T at(int index) const
+    {
+        if (index < 0 || index >= currentSize)
+        {
+            std::cout << "Index out of bounds." << std::endl;
+            return T();
+        }
+
+        return data[index];
+    }
+
+    int size() const
+    {
+        return currentSize;
+    }
+
+    T sum() const
+    {
+        T total = 0;
+
+        for (int i = 0; i < currentSize; i++)
+        {
+            total += data[i];
+        }
+
+        return total;
+    }
+
+    T max() const
+    {
+        if (currentSize == 0)
+        {
+            std::cout << "Array is empty." << std::endl;
+            return T();
+        }
+
+        T largest = data[0];
+
+        for (int i = 1; i < currentSize; i++)
+        {
+            if (data[i] > largest)
+            {
+                largest = data[i];
+            }
+        }
+
+        return largest;
+    }
+
+    T min() const
+    {
+        if (currentSize == 0)
+        {
+            std::cout << "Array is empty." << std::endl;
+            return T();
+        }
+
+        T smallest = data[0];
+
+        for (int i = 1; i < currentSize; i++)
+        {
+            if (data[i] < smallest)
+            {
+                smallest = data[i];
+            }
+        }
+
+        return smallest;
+    }
+
+    T* slice(int begin, int end) const
+    {
+        if (begin < 0 || end >= currentSize || begin > end)
+        {
+            std::cout << "Invalid slice bounds." << std::endl;
+            return nullptr;
+        }
+
+        int sliceSize = end - begin + 1;
+        T* slicedArray = new T[sliceSize];
+
+        for (int i = 0; i < sliceSize; i++)
+        {
+            slicedArray[i] = data[begin + i];
+        }
+
+        return slicedArray;
+    }
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "GenericArray.h"
+
+int main()
+{
+    GenericArray<int> int_array;
+
+    int_array.addElement(0);
+    int_array.addElement(2);
+    int_array.addElement(4);
+    int_array.addElement(6);
+    int_array.addElement(8);
+    int_array.addElement(10);
+    int_array.addElement(12);
+    int_array.addElement(14);
+    int_array.addElement(16);
+    int_array.addElement(18);
+    int_array.addElement(20);
+
+    std::cout << "Element at index 3 is " << int_array.at(3) << std::endl;
+    std::cout << "Size of array is " << int_array.size() << std::endl;
+    std::cout << "Sum of the array is " << int_array.sum() << std::endl;
+    std::cout << "Maximum and minimum of array is "
+              << int_array.min() << " and " << int_array.max() << std::endl;
+
+    int begin = 5;
+    int end = 10;
+    int sliceSize = end - begin + 1;
+
+    int* sliced_array = int_array.slice(begin, end);
+
+    if (sliced_array != nullptr)
+    {
+        std::cout << "Sliced array from index " << begin << " to " << end << ": ";
+
+        for (int i = 0; i < sliceSize; i++)
+        {
+            std::cout << sliced_array[i] << " ";
+        }
+
+        std::cout << std::endl;
+
+        delete[] sliced_array;
+    }
+
+    return 0;
+}
